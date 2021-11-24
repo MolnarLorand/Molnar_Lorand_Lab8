89471e95fe9abea4fa89ef6aa5af33f71242e4d2
MolnarLorand <andrevzo84@gmail.com>
2021-11-25 00:34:17 +0200

    Add project files.

---
+++ Data/Molnar_Lorand_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Molnar_Lorand_Lab8.Models;
+
+namespace Molnar_Lorand_Lab8.Data
+{
+    public class Molnar_Lorand_Lab8Context : DbContext
+    {
+        public Molnar_Lorand_Lab8Context (DbContextOptions<Molnar_Lorand_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Molnar_Lorand_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Molnar_Lorand_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Molnar_Lorand_Lab8.Models.Category> Category { get; set; }
+    }
+}
